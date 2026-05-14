1ef9eb33a43da0f77d1e887cd7b7d6bc953dbfa3
Mariiriik <latartsevamaria@gmail.com>
2026-05-14 04:05:25 -0700

    Add files via upload
    Выполнила настройку окружения, Spring Initializr, Spring Web, HelloController

---
+++ HelloController.java
@@ -0,0 +1,19 @@
+package com.example.latartsevaSpring1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Ура, мой первый rest-контроллер работает!!!!!";
+    }
+    @GetMapping("/status")
+    public String status () {
+        return "Application is running";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "Help Desk project by Latartseva M.D.";
+    }
+}
