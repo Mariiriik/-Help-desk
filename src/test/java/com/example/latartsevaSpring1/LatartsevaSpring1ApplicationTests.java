e2d503f0c42d24de2d824997390245bcfcf88dd4
Mariiriik <latartsevamaria@gmail.com>
2026-05-28 01:10:26 -0700

    Module 2: Thymeleaf and Bootstrap finished

---
+++ src/test/java/com/example/latartsevaSpring1/LatartsevaSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latartsevaSpring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatartsevaSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
